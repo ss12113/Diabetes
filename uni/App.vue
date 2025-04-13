f1a07cdacc71d2fd6f84f024eccced0d20b0c1b3
aiyawanan <145367600+ss12113@users.noreply.github.com>
2025-04-13 13:51:37 +0800

    Add files via upload

---
+++ uni/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <slot />
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+/* 全局 100% 宽高，去除默认 margin/padding */
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+/* 如果需要支持 iOS 刘海屏安全区 */
+#app {
+  padding-bottom: env(safe-area-inset-bottom);
+}
+</style>
